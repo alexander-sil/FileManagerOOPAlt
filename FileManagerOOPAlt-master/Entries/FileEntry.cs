f5520e6b483f655c9f5cd92050b6ece83ab1f34d
alexander-sil <93575945+alexander-sil@users.noreply.github.com>
2022-08-12 11:57:29 +0300

    Add files via upload

---
+++ FileManagerOOPAlt-master/Entries/FileEntry.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManagerOOPAlt.Entries
+{
+    public class FileEntry : Entry
+    {
+        // Конструктор
+        public FileEntry(FileInfo info)
+        {
+            Reference = info;
+
+            Size = info.Length;
+
+            Name = info.FullName;
+
+            Extension = info.Extension;
+
+            Attributes = info.Attributes;
+
+            LastWriteTime = info.LastWriteTime;
+
+            CreationTime = info.CreationTime;
+        }
+
+        // Ccылка на FileSystemInfo
+        public override FileSystemInfo Reference { get; set; }
+
+        // Имя
+        public override string Name { get; set; }
+
+        // Размер файла
+        public long Size { get; set; }
+
+        // Расширение файла
+        public string Extension { get; set; }
+
+        // Атрибуты файла формата DOS
+        public override FileAttributes Attributes { get; set; }
+
+        // Изменен:
+        public override DateTime LastWriteTime { get; set; }
+
+        // Создан:
+        public override DateTime CreationTime { get; set; }
+
+    }
+}
